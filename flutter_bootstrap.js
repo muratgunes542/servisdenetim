d0263bbee72bf69540d51993fad2680d20e84cce
muratgunes542 <muratgunes542@gmail.com>
2026-09-01 13:11:01 +0300

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1185565884"
+    serviceWorkerVersion: "4293313651"
   }
 });
